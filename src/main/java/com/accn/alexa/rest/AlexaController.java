57e6e0f822bacc50bedff8d48bb9364c3ebcddef
Jagdeesh-Murugesan <jagdeesh.murugesan@accenture.com>
2018-10-22 08:50:35 +0530

    Used Alexa SDK model classes

--- src/main/java/com/accn/alexa/rest/AlexaController.java
+++ src/main/java/com/accn/alexa/rest/AlexaController.java
@@ -1,49 +1,55 @@
 package com.accn.alexa.rest;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.accn.alexa.request.domain.AlexaRequest;
-import com.accn.alexa.response.domain.AlexaResponse;
-import com.accn.alexa.response.domain.Body;
-import com.accn.alexa.response.domain.OutputSpeech;
-import com.accn.alexa.response.domain.Response;
+import com.amazon.ask.model.RequestEnvelope;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.model.ResponseEnvelope;
+import com.amazon.ask.model.SessionEndedRequest;
+import com.amazon.ask.response.ResponseBuilder;
+
+
 
 @RestController
 public class AlexaController {
 	
+    Logger logger = LoggerFactory.getLogger(AlexaController.class);
 	
-	  @PostMapping("/skill")
-	public AlexaResponse process(@RequestBody AlexaRequest request) {
+	
+	@PostMapping("/skill")
+	public @ResponseBody ResponseEnvelope process(@RequestBody RequestEnvelope requestEnvlop) {
 
-		System.out.println("--- HCSC HealthCare ----" + request.getRequest().getType());
-
-		AlexaResponse alexaRes = new AlexaResponse();
-		Body body = new Body();
-		Response response = new Response();
-		OutputSpeech os = new OutputSpeech();
-
-		if ("LaunchRequest".equals(request.getRequest().getType())) {
-			// Set OutputSpeech values
-			os.setSsml("<speak>Welcome to India</speak>");
-			os.setType("SSML");
-
-			// Set Response
-			response.setOutputSpeech(os);
-			response.setShouldEndSession(false);
-		}
-		else if("SessionEndedRequest".equals(request.getRequest().getType())) {
+		Optional<Response> response;
+		String reqType = requestEnvlop.getRequest().getType();
+	    ResponseBuilder builder = new ResponseBuilder();
+		  
+	    logger.info("--- HCSC HealthCare ----" + reqType);
+		if("LaunchRequest".equals(reqType)) {
+			response = builder.withSpeech("Hi All, Welcome to offshore HCSC").withShouldEndSession(Boolean.TRUE).build();
 			
 		}
-		// Set body
-		body.setVersion("1.0");
-		body.setResponse(response);
-//		  body.setUserAgent("Apache-HttpClient/4.5.x (Java/1.8.0_172)");		  		  
-
-		alexaRes.setBody(body);
-
-		return alexaRes;
+		else if ("SessionEndedRequest".equals(reqType)) {
+			response = builder.withShouldEndSession(Boolean.TRUE).build();
+			SessionEndedRequest sr = (SessionEndedRequest) requestEnvlop.getRequest();
+			if (sr.getError() != null) {
+				logger.debug("---Error Msg--- " + sr.getError().getMessage());
+			}
+			if (sr.getReason() != null) {
+				logger.debug("---Reason--- " + sr.getReason().getValue());
+			}
+		}
+		else {
+			response = builder.withSpeech("Sorry, I dont know that").withShouldEndSession(Boolean.TRUE).build();
+		}
+		
+		return ResponseEnvelope.builder().withVersion("1.0").withResponse(response.get()).build();
 	}
 
 }
